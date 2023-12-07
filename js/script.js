00200844000339589c32c3d7f807f507a9a463cf
Maksym Ivashchenko <116831065+Moonamee@users.noreply.github.com>
2023-12-07 03:36:39 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,31 +1,53 @@
 $(".popup").css("display", "none");
 
 // BURGER MENU-------------------------------------------
-let menuBtn = document.querySelector('.btn-burger');
-let menu = document.querySelector('.menu-burger');
+let menuBtn = document.querySelector(".btn-burger");
+let menu = document.querySelector(".menu-burger");
 
-menuBtn.addEventListener('click', function() {
-	menuBtn.classList.toggle('active');
-	menu.classList.toggle('active');
+menuBtn.addEventListener("click", function () {
+  menuBtn.classList.toggle("active");
+  menu.classList.toggle("active");
 });
 
 // PORTFOLIO GALLERY ---------------------------------
-$('.portfolio-gallery-box img').click(function() {
-    let src = $(this).attr('src');
-    $('.popup img').attr('src',src);
-    $('.popup').fadeIn();
+$(".portfolio-gallery-box img").click(function () {
+  let src = $(this).attr("src");
+  $(".popup img").attr("src", src);
+  $(".popup").fadeIn();
 });
 
-$('.close').click(function() {
-    $('.popup').fadeOut();
+$(".close").click(function () {
+  $(".popup").fadeOut();
 });
-
 
 // POPUP CLICK CLOSER OBJECT-------------------
-$('.popup-wrap').click(function() {
-    $('.popup').fadeOut();
+$(".popup-wrap").click(function () {
+  $(".popup").fadeOut();
 });
 
+// Search
+function toggleSearchInput() {
+  var searchInput = document.querySelector(".search_input");
+  var searchImg = document.querySelector(".search_img");
 
+  if (searchInput.style.display === "block") {
+    searchInput.style.display = "none";
+    searchImg.classList.remove("clicked");
+  } else {
+    searchInput.style.display = "block";
+    searchImg.classList.add("clicked");
+  }
+}
 
+document.addEventListener("click", function (event) {
+  var searchInput = document.querySelector(".search_input");
+  var searchImg = document.querySelector(".search_img");
 
+  if (
+    !searchInput.contains(event.target) &&
+    !searchImg.contains(event.target)
+  ) {
+    searchInput.style.display = "none";
+    searchImg.classList.remove("clicked");
+  }
+});
